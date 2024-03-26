fe5369b56854cb4e88150d5512bf5937f25a5f0a
GuilhermeCeglio <guilhermeaceglio02@gmail.com>
2024-03-26 16:37:23 -0300

    console log indicando modificações de guilherme.

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("A aplicação esta se conectando ao banco de dados.")
+// mais um console log de teste
+console.log("O guilherme fez esse novo commmit !!!")
